eed386f4cda00cb25b01081cc5b1cb32599ec84c
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-06-12 16:45:48 +0200

    UsingMonoZip

---
+++ src/main/java/com/jsharper/mono/UsingMonoZip.java
@@ -0,0 +1,78 @@
+package com.jsharper.mono;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import com.jsharper.utils.Utils;
+
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuple3;
+import reactor.util.function.Tuple4;
+import reactor.util.function.Tuple5;
+import reactor.util.function.Tuple6;
+import reactor.util.function.Tuple7;
+import reactor.util.function.Tuple8;
+
+public class UsingMonoZip {
+
+	public static void main(String[] args) {
+		UsingMonoZip zip = new UsingMonoZip();
+
+		Function<Object[], String> combiner = (values) -> {
+			Arrays.asList(values).forEach(System.out::println);
+			return "Hello World";
+		};
+		
+		zip.zip(combiner, Mono.just(1042)).subscribe(Utils.onNext(), Utils.onError(), Utils.onComplete());
+		
+		zip.zip(Mono.just("first"), Mono.just("second")).subscribe(Utils.onNext(), Utils.onError(), Utils.onComplete());
+		
+		zip.zip(Mono.just("first"), Mono.just("second"), Mono.just("third") ).subscribe(Utils.onNext(), Utils.onError(), Utils.onComplete());
+	}
+
+	public Mono<String> zip(Function<Object[], String> combiner, Mono<Integer> source) {
+		return Mono.zip(combiner, source);
+	}
+
+	public Mono<String> zip(List<Mono<String>> monos, Function<Object[], String> combiner) {
+		return Mono.zip(monos, combiner);
+	}
+
+	public Mono<Tuple2<String, String>> zip(Mono<String> mono1, Mono<String> mono2) {
+		return Mono.zip(mono1, mono2);
+	}
+
+	public Mono<Tuple3<String, String, String>> zip(Mono<String> mono1, Mono<String> mono2, Mono<String> mono3) {
+		return Mono.zip(mono1, mono2, mono3);
+	}
+
+	public Mono<Tuple4<String, String, String, String>> zip(Mono<String> mono1, Mono<String> mono2, Mono<String> mono3,
+			Mono<String> mono4) {
+		return Mono.zip(mono1, mono2, mono3, mono4);
+	}
+
+	public Mono<Tuple5<String, String, String, String, String>> zip(Mono<String> mono1, Mono<String> mono2,
+			Mono<String> mono3, Mono<String> mono4, Mono<String> mono5) {
+		return Mono.zip(mono1, mono2, mono3, mono4, mono5);
+	}
+
+	public Mono<Tuple6<String, String, String, String, String, String>> zip(Mono<String> mono1, Mono<String> mono2,
+			Mono<String> mono3, Mono<String> mono4, Mono<String> mono5, Mono<String> mono6) {
+		return Mono.zip(mono1, mono2, mono3, mono4, mono5, mono6);
+	}
+
+	public Mono<Tuple7<String, String, String, String, String, String, String>> zip(Mono<String> mono1,
+			Mono<String> mono2, Mono<String> mono3, Mono<String> mono4, Mono<String> mono5, Mono<String> mono6,
+			Mono<String> mono7) {
+		return Mono.zip(mono1, mono2, mono3, mono4, mono5, mono6, mono7);
+	}
+
+	public Mono<Tuple8<String, String, String, String, String, String, String, String>> zip(Mono<String> mono1,
+			Mono<String> mono2, Mono<String> mono3, Mono<String> mono4, Mono<String> mono5, Mono<String> mono6,
+			Mono<String> mono7, Mono<String> mono8) {
+		return Mono.zip(mono1, mono2, mono3, mono4, mono5, mono6, mono7, mono8);
+	}
+
+}
